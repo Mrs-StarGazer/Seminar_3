d278d8e232e2926e0351577324290b2576063851
Maria <m.shklyaeva96@gmail.com>
2022-12-10 21:53:45 +0500

    add task 19

---
+++ task_19/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("введите число");
+int number = int.Parse(Console.ReadLine()!);
+
+if (10000<=number && number <= 99999)
+{
+    int digit1=number/10000;
+    int digit2=number/1000%10;
+    int digit4=number/10%10;
+    int digit5=number%10;
+
+    if (digit1==digit5 &&digit2==digit4)
+    {
+        Console.WriteLine ("число палиндром");
+    }
+    else
+    {
+        Console.WriteLine ("число не палиндром");
+    }
+}
