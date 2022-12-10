a9dbb00de4bac889856481cc7d2b33cb1d303fe9
Maria <m.shklyaeva96@gmail.com>
2022-12-10 22:23:41 +0500

    add task 21

---
+++ task_21/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine ("введите Х координат первой точки");
+int x1=int.Parse(Console.ReadLine ()!);
+Console.WriteLine ("введите y координат первой точки");
+int y1=int.Parse(Console.ReadLine ()!);
+Console.WriteLine ("введите z координат первой точки");
+int z1=int.Parse(Console.ReadLine ()!);
+Console.WriteLine ("введите y координат второй точки");
+int x2=int.Parse(Console.ReadLine ()!);
+Console.WriteLine ("введите Х координат второй точки");
+int y2=int.Parse(Console.ReadLine ()!);
+Console.WriteLine ("введите z координат второй точки");
+int z2=int.Parse(Console.ReadLine ()!);
+
+double sqrt=Math.Sqrt (Math.Pow (x1-x2, 2)+ Math.Pow (y1-y2, 2) + Math.Pow (z1-z2, 2));
+
+Console.WriteLine (sqrt);
